e716302200571d9743cb8323202e002c105cda70
Mihir Brahmaniya <119410418+meet2121@users.noreply.github.com>
2025-12-15 22:34:51 +0530

    Added print statement on GitHub

--- hello_world.py
+++ hello_world.py
@@ -3,7 +3,7 @@ class HelloWorld:
     @staticmethod
     def main():
         print("Hello World from GitHub")
-
+        print("This line was added on GitHub")
 
 if __name__ == "__main__":
     HelloWorld.main()
